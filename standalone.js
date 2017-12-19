4b91108da606c0a5416de6583ba0b0681998607c
Alex Bowen <ajbowen249@gmail.com>
2017-12-19 13:34:12 -0500

    update default port

--- standalone.js
+++ standalone.js
@@ -2,5 +2,5 @@ const coreServer = require('bplc-node-server');
 const RPICore = require('./rpiCore').RPICore;
 
 const rpiCore = new RPICore();
-const server = new coreServer.server(rpiCore);
+const server = new coreServer.server(rpiCore, 8081);
 server.init();
